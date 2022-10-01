e734cd639d4eded51f7df91fab8981c24e37505f
Mahendra1033 <mahendramahiee1033@gmail.com>
2022-10-01 12:17:44 +0100

    initial commit - Project created

---
+++ src/test/java/com/golearntolead/git/SpringGitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golearntolead.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
