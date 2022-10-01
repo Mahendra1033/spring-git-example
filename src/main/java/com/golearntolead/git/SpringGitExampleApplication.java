1dde40032d39dd0e3fa3588525697a8f055cc5ab
Mahendra1033 <mahendramahiee1033@gmail.com>
2022-10-01 13:03:47 +0100

    declared the enableswagger2 anno inside main class

--- src/main/java/com/golearntolead/git/SpringGitExampleApplication.java
+++ src/main/java/com/golearntolead/git/SpringGitExampleApplication.java
@@ -2,8 +2,10 @@ package com.golearntolead.git;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringGitExampleApplication {
 
 	public static void main(String[] args) {
