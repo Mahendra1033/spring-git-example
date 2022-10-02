08df6355d28767abe0cd3d6fa0210d7a33ae0ed5
Mahendra1033 <mahendramahiee1033@gmail.com>
2022-10-02 12:28:16 +0100

    made the changes and implemented the response data

--- src/main/java/com/golearntolead/git/controller/StudentController.java
+++ src/main/java/com/golearntolead/git/controller/StudentController.java
@@ -1,8 +1,8 @@
 package com.golearntolead.git.controller;
 
 import com.golearntolead.git.entity.Student;
+import com.golearntolead.git.response.ResponseData;
 import com.golearntolead.git.service.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,7 +14,7 @@ public class StudentController {
      StudentService studentService = new StudentService();
 
     @GetMapping("/getStudents")
-    public List<Student> getStudents(){
+    public List<ResponseData> getStudents(){
         return studentService.getAllStudentDetails();
     }
 
